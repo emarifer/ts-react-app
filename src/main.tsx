76bb26b756ca0f3b0cc24dd57d46a43d4cc8a3e3
emarinfer <enriquemarin_sierra@hotmail.com>
2021-12-03 16:05:45 +0100

    agregado nueva config eslint y comentarios

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,30 @@ ReactDOM.render(
 );
 
 /**
- * dOCS REACT-ROUTER-DOM. VER:
+ * DOCS REACT-ROUTER-DOM. VER:
  * https://reactrouter.com/docs/en/v6
+ *
+ * TYPESCRIPT-PLUGIN-CSS-MODULES. VER:
+ * https://www.npmjs.com/package/typescript-plugin-css-modules
+ *
+ * TIPO «NEVER» (NUNCA). VER:
+ * https://apuntes.de/typescript/never/#gsc.tab=0
+ *
+ * CAN WE HAVE A RETURN STATEMENT IN A JAVASCRIPT SWITCH STATEMENT?. VER:
+ * https://www.tutorialspoint.com/can-we-have-a-return-statement-in-a-javascript-switch-statement#:~:text=The%20JavaScript%20switch%20statement%20can,statement%20will%20not%20be%20executed.
+ *
+ * ERROR IDENTIFICACION DE TYPESCRIPT. VER:
+ * https://stackoverflow.com/questions/50432556/cannot-use-jsx-unless-the-jsx-flag-is-provided
+ * https://code.visualstudio.com/docs/typescript/typescript-tutorial
+ *
+ * TYPESCRIPT-REACT. VER:
+ * https://www.typescriptlang.org/docs/handbook/react.html
+ *
+ * SETTINGS ESLINT WITH TYPESCRIPT. VER:
+ * https://andrebnassis.medium.com/setting-eslint-on-a-react-typescript-project-2021-1190a43ffba
+ * https://www.enbonnet.me/post/usar-eslint-y-prettier-en-proyectos-typescript
+ * https://dev.to/omarbastos/configurando-react-ts-con-eslint-prettier-en-2021-12ho
+ *
+ * HOOKS DE REACT Y TYPESCRIPT. VER:
+ * https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/hooks
  */
